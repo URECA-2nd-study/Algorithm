1fa8520a669eb4ba67a67ffb23b1ec773b3e2651
ParkKiJung <105425330+ParkKiJung@users.noreply.github.com>
2025-02-19 16:28:25 +0900

    [level 3] Title: 입국심사, Time: 57.04 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        long left = 1;
+        long right = (long) Arrays.stream(times).max().getAsInt() * n;
+        
+        while(left < right) {
+            long mid = (left + right) / 2;
+            if (check(mid, n, times)) right = mid;
+            else left = mid + 1;
+        }
+        
+        return left;
+    }
+    
+    static boolean check(long mid, int n, int[] times) {
+        long cnt = 0;
+        
+        for (int i = 0; i < times.length; i++) {
+            cnt += mid / times[i];
+        }
+        
+        return cnt >= n;
+    }
+    
+}
