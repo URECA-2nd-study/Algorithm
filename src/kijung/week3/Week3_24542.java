5310b889f35788ccb39794ffd5cfbc2ed52dd62a
ParkKiJung <pkj862491@gmail.com>
2025-02-20 11:28:57 +0900

    [feat] week3

---
+++ src/kijung/week3/Week3_24542.java
@@ -0,0 +1,62 @@
+package BaekJoon_24542;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] parent;
+    static Map<Integer, Integer> map = new HashMap<>();
+    static final int MOD = 1_000_000_007;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        parent = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            union(a, b);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            int root = find(i); // 부모 찾기
+            map.put(root, map.getOrDefault(root, 0) + 1); // 부모가 기존에 없던 값이라면 0으로 설정 후 증가. 있던 값이라면 바로 증가
+        }
+
+        long answer = 1;
+        for (int size : map.values()) {
+            answer = (answer * size) % MOD;
+        }
+
+        System.out.println(answer);
+    }
+
+    static int find(int a) {
+        if (parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
+    }
+
+    static void union(int a, int b) {
+        int aRoot = find(a);
+        int bRoot = find(b);
+
+        if (aRoot != bRoot) {
+            parent[aRoot] = bRoot;
+        }
+    }
+}
