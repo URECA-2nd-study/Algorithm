e9b183145e98406ce0065045f4c77521a518297d
ParkKiJung <105425330+ParkKiJung@users.noreply.github.com>
2025-02-20 10:50:36 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1540 ms, Memory: 329304 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,86 +1,89 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static Edge[] edges;
-    static int[] parents;
+
+    static int[] parent;
+    static ArrayList<Edge> edges = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        edges = new Edge[M];
-        parents = new int[N + 1];
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        parent = new int[N + 1];
+        init();
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            edges[i] = new Edge(from, to, cost);
+            int weight = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(from, to, weight));
         }
 
-        Arrays.sort(edges);
-        init();
-        System.out.println(kruskal());
+        Collections.sort(edges);
+
+        System.out.println(mst(N));
+
     }
 
-    static int kruskal() {
-        int selectedEdge = 0;
-        int totalCost = 0;
+    static int mst(int N) {
+        int sum = 0;
         int max = 0;
+        int selectedEdge = 0;
 
-        for (int i = 0; i < M; i++) {
-            Edge curEdge = edges[i];
-            if (find(curEdge.from) != find(curEdge.to)) {
+        for (Edge edge : edges) {
+
+            if (find(edge.from) != find(edge.to)) {
+                max = Math.max(max, edge.weight);
+                sum += edge.weight;
                 selectedEdge++;
-                totalCost += curEdge.cost;
-                union(curEdge.from, curEdge.to);
-                max = Math.max(max, curEdge.cost);
+                union(edge.from, edge.to);
             }
 
-            if (selectedEdge == N - 1) return totalCost - max;
-        }
-        return -1;
-    }
+            if (selectedEdge == N - 1) return sum - max;
 
-    static void init() {
-        for (int i = 1; i < N + 1; i++) {
-            parents[i] = i;
         }
+
+        return sum;
+
+    }
+    
+    static int find(int a) {
+        if (parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
     }
 
     static void union(int a, int b) {
         int aRoot = find(a);
         int bRoot = find(b);
-        parents[bRoot] = aRoot;
+        parent[aRoot] = bRoot;
     }
 
-    static int find(int a) {
-        if (parents[a] == a) return a;
-        else return parents[a] = find(parents[a]);
+    static void init() {
+        for (int i = 1; i < parent.length; i++) {
+            parent[i] = i;
+        }
     }
 }
 
 class Edge implements Comparable<Edge> {
     int from;
     int to;
-    int cost;
+    int weight;
 
-    public Edge(int from, int to, int cost) {
+    public Edge(int from, int to, int weight) {
         this.from = from;
         this.to = to;
-        this.cost = cost;
+        this.weight = weight;
     }
 
-
     @Override
-    public int compareTo(Edge o) {
-        return this.cost - o.cost;
+    public int compareTo(Edge e) {
+        return this.weight - e.weight;
     }
 }
