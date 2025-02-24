7c22bf6bedfbba9afbfad82d132dde594e9bd0b1
ParkKiJung <105425330+ParkKiJung@users.noreply.github.com>
2025-02-24 12:37:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.97 ms, Memory: 97.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String c : completion) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        
+        for (String p : participant) {
+            if (!map.containsKey(p) || map.get(p) <= 0) answer = p;
+            else map.put(p, map.get(p) - 1);
+        }
+        
+        return answer;
+    }
+}
