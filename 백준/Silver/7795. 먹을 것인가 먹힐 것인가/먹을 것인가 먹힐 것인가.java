df576ea8ebbd8295e855a4e92687d0151e6b70cf
ParkKiJung <105425330+ParkKiJung@users.noreply.github.com>
2025-02-26 13:32:12 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 436 ms, Memory: 35856 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] a, b;
+    static int sizeA, sizeB;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            sizeA = Integer.parseInt(st.nextToken());
+            sizeB = Integer.parseInt(st.nextToken());
+
+            a = new int[sizeA];
+            b = new int[sizeB];
+
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < sizeA; j++) {
+                a[j] = Integer.parseInt(st.nextToken());
+            }
+
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < sizeB; j++) {
+                b[j] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(a);
+            Arrays.sort(b);
+
+            int answer = 0;
+            for (int j = 0; j < a.length; j++) {
+                int target = a[j];
+                answer += find(target);
+            }
+
+            bw.write(answer + "\n");
+
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    static int find(int target) {
+        int left = 0;
+        int right = sizeB;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (b[mid] >= target) right = mid;
+            else left = mid + 1;
+        }
+
+        return left;
+
+    }
+}
