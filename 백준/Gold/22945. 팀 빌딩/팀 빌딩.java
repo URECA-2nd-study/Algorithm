933a92cdb3e4fb891f3fb973da2ec5f96fc9e87b
ParkKiJung <105425330+ParkKiJung@users.noreply.github.com>
2025-02-26 11:37:02 +0900

    [Gold IV] Title: 팀 빌딩, Time: 264 ms, Memory: 24260 KB -BaekjoonHub

---
+++ 백준/Gold/22945. 팀 빌딩/팀 빌딩.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] nums = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 포인터를 양끝에 위치시키면서 줄이는건 어떨지...?
+        int left = 0;
+        int right = N - 1;
+        int answer = 0;
+
+
+        while (left < right) {
+
+            int len = right - left - 1;
+            answer = Math.max(answer, Math.min(nums[left], nums[right]) * len);
+
+            if (nums[left] <= nums[right]) left++;
+            else right--;
+
+        }
+
+        System.out.println(answer);
+
+    }
+}
