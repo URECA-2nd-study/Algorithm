d1e870ccb3324cb6896176098764694747b84475
Jinu Yi <dionisos198@naver.com>
2025-03-13 14:48:29 +0900

    [feat] No.17142

--- src/jinu/week6/Baekjoon_17142.java
+++ src/jinu/week6/Baekjoon_17142.java
@@ -15,6 +15,7 @@ public class Baekjoon_17142 {
     static int dy[]= {-1,1,0,0};
     static int dx[] = {0,0,-1,1};
 
+    // 빈 칸중 바이러스에 감염된 것을 판단하기 위한 배열
     static boolean isVisited[][];
 
     static int N;
@@ -32,6 +33,8 @@ public class Baekjoon_17142 {
         map = new int[N][N];
         isVisited = new boolean[N][N];
 
+
+        // 비활성 바이러스의 리스트, 활성 바이러스의 후보군을 List 에 담아 놓음.
         List<int[]> activateVirusCandidate = new ArrayList<>();
 
         for(int i=0;i<N;i++){
@@ -54,6 +57,7 @@ public class Baekjoon_17142 {
 
     }
 
+    // 백트래킹 방법으로 비활성 바이러스 중 m 개의 활성 바이러스 선태
     public static void BT(List<int[]> activateVirusCandidate, List<int[]> activateVirus , int m ,int start){
         if(activateVirus.size()==m){
 
@@ -68,17 +72,21 @@ public class Baekjoon_17142 {
         }
     }
 
+    // BFS 방식으로 활성 상태의 바이러스로부터 시작해서 전파.
     public static void BFS(List<int[]> activateVirus){
 
+        // 초기화
         intializeIsVisited();
         Queue<int[]> queue = new LinkedList<>();
         int time = 0;
 
+        // 활성 바이러스를 Queue 에 담음.
         activateVirus.forEach(position ->{
             queue.add(new int[]{position[0],position[1],time});
             isVisited[position[0]][position[1]] = true;
         });
 
+        // 모든 빈칸에 전파하는 시간
         int timeMax = 0;
 
         while(!queue.isEmpty()){
@@ -91,6 +99,8 @@ public class Baekjoon_17142 {
                 int newX = virusInfo[1] + dx[k];
 
                 if(newY>=0 && newY < N && newX>=0 && newX< N && !isVisited[newY][newX] && (map[newY][newX] == 0 || map[newY][newX] ==2)){
+
+                    // 빈 칸이라면 시간을 1 증가한 것을 최대 TIME 으로 갱신
                     if(map[newY][newX]==0){
                         timeMax = Math.max(timeMax,virusInfo[2]+1);
                     }
@@ -103,6 +113,7 @@ public class Baekjoon_17142 {
         }
 
 
+        // 모든 빈칸에 바이러스 가 퍼졌는지 판단.
         if(isAllSpread()){
             ANSWER = Math.min(ANSWER,timeMax);
         }
